b5785c92b64158fecafd3567c8f263d2158f05cb
Tiago de Tarso Raggiotto Gonçalves <97037591+tiagodetarso@users.noreply.github.com>
2022-07-26 11:13:04 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -202,7 +202,6 @@ BDC_MAss.canta_pra_mim()
 
 # EXERCÍCIO 08
 from datetime import datetime
-
 class JatoMilitar1Lugar:
     """Classe que representa Jatos Militares de um lugar"""
     def __init__(self, modelo, base_orig):
@@ -237,3 +236,44 @@ jato1 = JatoMilitar1Lugar('TT16', 'Curitiba')
 jato1.designar_piloto('Francisco José')
 jato1.rebasear_aeronave('São Paulo')
 print(jato1)
+
+
+
+# EXERCÍCIO 09:
+from datetime import datetime
+class JatoMilitar2Lugares:
+    """Classe que representa Jatos Militares de um lugar"""
+    def __init__(self, modelo, base_orig):
+        self.modelo = modelo
+        self.base = base_orig
+        self._historico = [self.base]
+        self._designa = False
+    
+    def designar_pilotos(self, piloto, co_piloto):
+        self.piloto = piloto
+        self.co_piloto = co_piloto
+        self._designa = True
+    
+    def rebasear_aeronave(self, base_nova):
+        if self._designa:
+            self.base = base_nova
+            self._data = datetime.now().strftime('%d-%m-%Y')
+            self._hora = datetime.now().strftime('%H:%M')
+            self._historico.append([self.base, self._data, self._hora])
+            self._designa = False
+        
+        else:
+            return print("Não foi possível rebasear a aeronave pois não há piloto designado.")
+    
+    def __str__(self):
+        return """ 
+                Jato: %s
+                Base de Origem: %s
+                Pilotos: %s e %s
+                Histórico de Transferências: %s
+                """ % (self.modelo, self._historico[0], self.piloto, self.co_piloto, self._historico[1::])
+
+jato2 = JatoMilitar2Lugares('RG07', 'Curitiba')
+jato2.designar_pilotos('Edson Arantes do Nascimento','Ronaldo de Assis' )
+jato2.rebasear_aeronave('São Paulo')
+print(jato2)
